0af46f15a357c748b1526dd2a5afbf3f21173b21
surmoo <166159664+surmoo@users.noreply.github.com>
2026-04-10 02:16:11 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,9 @@
-const CACHE = 'expocap-v3';
+const CACHE = 'expocap-v5'; // Поменял версию кэша, чтобы телефон обновил файлы
 const ASSETS = [
+  './',
   './index.html',
   './manifest.json',
+  './jszip.min.js', // <-- Теперь библиотека качается в память телефона
   './sw.js'
 ];
 
@@ -9,12 +11,14 @@ self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
   self.skipWaiting();
 });
+
 self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(keys =>
     Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
   ));
   self.clients.claim();
 });
+
 self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request).then(r => r || fetch(e.request))
